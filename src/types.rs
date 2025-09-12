240f3d763ecacf6c3a73ed87b8c30530f33cbfd1
dmcarthur <dmcarthur@mathematica-mpr.com>
2025-09-12 18:21:16 -0500

    fix response deserialization

--- src/types.rs
+++ src/types.rs
@@ -6,19 +6,21 @@ pub struct OpenChannelResponse {
     pub channel_status: ChannelStatus,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Default)]
+#[serde(default)]
 pub struct ChannelStatus {
-    channel_status_code: String,
-    pub last_committed_offset_token: Option<u64>,
     database_name: String,
     schema_name: String,
     pipe_name: String,
     channel_name: String,
+    channel_status_code: String,
+    pub last_committed_offset_token: Option<String>,
+    created_on_ms: u64,
     rows_inserted: i32,
     rows_parsed: i32,
     rows_errors: i32,
-    last_error_offset_upper_bound: String,
-    last_error_message: String,
-    last_error_timestamp: String, // timestamp_utc
+    last_error_offset_upper_bound: Option<String>,
+    last_error_message: Option<String>,
+    last_error_timestamp: Option<u64>, // timestamp_utc
     snowflake_avg_processing_latency_ms: i32,
 }
